dc0000ba97fb8e7dec7093e62a8250642de700a2
Shubham Singh <shubhamsingh.s8425@gmail.com>
2025-04-06 16:43:24 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,5 @@ export function greet(name: string): string {
     return `Hello, ${name}!`;
   }
   
-  console.log(greet("Jenkins Pipeline assignment. We've reached till pull request handling step"));
+  console.log(greet("Jenkins Pipeline assignment. We've reached till pull request handli"));
   
